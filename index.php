be2c2d37ccc860bff820c720c8b1b74f336184c4
Kristy Ramage <kristy.ramage@gmail.com>
2016-01-27 14:30:09 +1300

    created template files to tidy up code

--- index.php
+++ index.php
@@ -1,38 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Shopping Cart</title>
-</head>
-<body>
+<?php  include'templates/header.template.php'; ?>
 
-	<h1>Products</h1>
+<h1>Products</h1>
 
-	<?php 
+<?php 
 
-		//Connect to the database
-		$dbc = new mysqli('localhost', 'root', '', 'shopping_cart');
+	//Connect to the database
+	$dbc = new mysqli('localhost', 'root', '', 'shopping_cart');
 
-		// Get all products from the database
-		$sql = "	SELECT id, name, description, price, stock FROM products";
+	// Get all products from the database
+	$sql = "	SELECT id, name, description, price, stock FROM products";
 
-		// Run the query
-		$result = $dbc->query( $sql );
+	// Run the query
+	$result = $dbc->query( $sql );
 
-		// Loop over the result
-		while ( $row = $result->fetch_assoc() ) {
+	// Loop over the result
+	while ( $row = $result->fetch_assoc() ) {
 			
-			// Present the data
-			echo '<ul>';
-			echo '<li>ID: '.$row['id'].'</li>';
-			echo '<li>Name: '.$row['name'].' </li>';
-			echo '<li>Description: '.$row['description'].' </li>';
-			echo '<li>Price: '.$row['price'].' </li>';
-			echo '<li>Stock: '.$row['stock'].' </li>';
-			echo '</ul>' ;
-		}
-	 ?>
+		include'templates/product.template.php';
+	}
+ ?>
 
+<?php  include'templates/footer.template.php'; ?>
 
-</body>
-</html>
